134b6536ce70ab301216fc0d26aeaac5f37754e0
honu5 <liranso@gmail.com>
2025-08-13 10:20:07 -0700

    the color issue is fixed

--- broker-website/backend/src/controllers/chatSyst/chat-frontend/src/index.js
+++ broker-website/backend/src/controllers/chatSyst/chat-frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 
 // If you want to keep web vitals reporting, import it correctly
